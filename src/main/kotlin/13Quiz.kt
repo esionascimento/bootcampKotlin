e860569636a295848d706ddc73732da16c2c2cb9
esionascimento <esionascimento@proton.me>
2023-02-05 01:24:01 -0400

    14 funções compactas

--- src/main/kotlin/13Quiz.kt
+++ src/main/kotlin/13Quiz.kt
@@ -1,13 +1,13 @@
-fun main(args: Array<String>) {
-  whatShouldIDoToday("asdf")
-}
-
-fun whatShouldIDoToday(
-  mood: String, weather: String = "sunny", temperature: Int = 24
-) {
-  if (mood == "happy" && weather == "Sunny") {
-    println("go for a walk")
-  } else {
-    println("Stay home and read.")
-  }
-}
+//fun main(args: Array<String>) {
+//  whatShouldIDoToday("asdf")
+//}
+//
+//fun whatShouldIDoToday(
+//  mood: String, weather: String = "sunny", temperature: Int = 24
+//) {
+//  if (mood == "happy" && weather == "Sunny") {
+//    println("go for a walk")
+//  } else {
+//    println("Stay home and read.")
+//  }
+//}
--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -171,3 +171,54 @@ import java.util.*
 //fun canAddFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true): Boolean {
 //  return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 //}
+// -- 14. Funções compactas
+fun main(args: Array<String>) {
+  println("Hello, ${args[0]}!")
+  feedTheFish()
+}
+
+fun feedTheFish() {
+  val day = randomDay()
+  val food = fishFood(day)
+  println("Today is $day and the fish eat $food")
+  shouldChangeWater(day, 20, 50)
+  shouldChangeWater(day)
+  shouldChangeWater(day, dirty = 50)
+  if (shouldChangeWater(day)) {
+    println("Change the water today")
+  }
+}
+
+fun shouldChangeWater(
+  day: String,
+  temperature: Int = 32,
+  dirty: Int = 20
+): Boolean {
+  return when {
+    isTooHot(temperature) -> true
+    isDirty(dirty) -> true
+    isSunday(day) -> true
+    else -> false
+  }
+}
+fun isTooHot(temperature: Int) = temperature > 30
+fun isDirty(dirty: Int) = dirty > 30
+fun isSunday(day: String) = day == "Sunday"
+
+fun randomDay(): String {
+  val week = listOf("Monday", "Tuesday", "Wednesday", "Friday", "Saturday", "Sunday")
+  return week[Random().nextInt(7)]
+}
+
+fun fishFood(day: String): String {
+  var food = "fasting"
+  return when (day) {
+    "Monday" -> "flakes"
+    "Wednesday" -> "redworms"
+    "Thursday" -> "granules"
+    "Friday" -> "mosquitoes"
+    "Saturday" -> "lettuce"
+    "Sunday" -> "plankton"
+    else -> "fasting"
+  }
+}
